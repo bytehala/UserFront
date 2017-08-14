84ce5d191f26724c3072d26f6cad804dfb7732c0
Lem Dulfo <lemuel.dulfo@gmail.com>
2017-08-15 06:14:05 +0800

    Signup mappings

--- src/main/java/com/userfront/controller/HomeController.java
+++ src/main/java/com/userfront/controller/HomeController.java
@@ -1,7 +1,13 @@
 package com.userfront.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.userfront.domain.User;
 
 @Controller
 public class HomeController {
@@ -15,5 +21,23 @@ public class HomeController {
 	public String index() {
 		return "index";
 	}
+	
+	@GetMapping("/signup")
+	public String signup(Model model) {
+		User user = new User();
+		model.addAttribute("user", user);
+		
+		return "signup";
+	}
+	
+	@PostMapping("/signup")
+	public String signupPost(@ModelAttribute("user") User user, Model model) {
+		
+//		if(userService.checkUserExists(user.getUserName(), user.getEmail())) {
+//			model.addAttribute(arg0, arg1)
+//		}
+		return "";
+	}
+	
 
 }
