39b2175eade725b523433874ac5be3476c49b4dd
Lem Dulfo <lemuel.dulfo@gmail.com>
2018-06-12 10:29:47 -0600

    Add UserService methods

--- src/main/java/com/userfront/dao/UserDao.java
+++ src/main/java/com/userfront/dao/UserDao.java
@@ -6,5 +6,9 @@ import com.userfront.domain.User;
 
 
 public interface UserDao extends CrudRepository<User, Long>{
+	
+	// By using this pattern "find + By + Column", spring boot generates the SQL query automagically
+	User findByUsername(String username);
+	User findByEmail(String email);
 
 }
--- src/main/java/com/userfront/service/impl/UserServiceImpl.java
+++ src/main/java/com/userfront/service/impl/UserServiceImpl.java
@@ -1,10 +1,40 @@
 package com.userfront.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.userfront.dao.UserDao;
+import com.userfront.domain.User;
 import com.userfront.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
+	
+	@Autowired
+	private UserDao userDao;
+	
+	public void save(User user) {
+		userDao.save(user);
+	}
+	
+	public User findByUsername(String username) {
+		return userDao.findByUsername(username);
+	}
+	
+	public User findByEmail(String email) {
+		return userDao.findByEmail(email);
+	}
+	
+	public boolean checkUserExists(String username, String email) {
+		return checkUserNameExists(username) || checkEmailExists(email);
+	}
+
+	public boolean checkEmailExists(String email) {
+		return findByEmail(email) == null;
+	}
+
+	public boolean checkUserNameExists(String username) {
+		return findByUsername(username) == null;
+	}
 
 }
